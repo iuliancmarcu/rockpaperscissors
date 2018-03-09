8ed97f7f1f4cc71039836045c5bbd41311922ac5
Iulian Marcu <iulian.marcu@ovoenergy.com>
2018-03-09 15:48:48 +0000

    Added tmp

---
+++ create-release-tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+old_version="$(git describe --tags --abbrev=0)"
+regex="^v([0-9]+)$"
+
+if [[ $old_version =~ $regex ]]
+then
+	new_version="v$((BASH_REMATCH[1]+1))"
+else
+	new_version="v1"
+fi
+
+git tag -a $new_version -m "$new_version released @ $(date --utc +%FT%T.%3NZ)"
+git push origin $new_version
